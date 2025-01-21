c1f542d7f3643e8785304360a3604d359ebb1d3d
chamathk77 <152027040+chamathk77@users.noreply.github.com>
2025-01-22 01:54:25 +0530

    add animation

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,94 +1,130 @@
 import 'package:flutter/material.dart';
 import 'package:meal_category_app_flutter/models/meal.dart';
-// import 'package:meal_category_app_flutter/providers/meals_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meal_category_app_flutter/providers/favourite_provider.dart';
-import 'package:meal_category_app_flutter/providers/filter_provider.dart';
 
 class MealDetailsScreen extends ConsumerWidget {
   const MealDetailsScreen({
     super.key,
     required this.meal,
-    // required this.onToggleFavorite,
   });
 
   final Meal meal;
-  // final void Function(Meal meal) onToggleFavorite;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final favouriteMeals = ref.watch(filteredMealsProvider);
-    final isFavorite = favouriteMeals.contains(meal);
-    return Scaffold(
-        appBar: AppBar(title: Text(meal.title), actions: [
-          IconButton(
-              onPressed: () {
-                final isExisting = ref
-                    .read(favoriteMealsProvider.notifier)
-                    .toggleMealFavoriteStatus(meal);
+    // Watch the favorite meals provider
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
 
-                ScaffoldMessenger.of(context).clearSnackBars();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(
-                      isExisting
-                          ? 'Meal added to favorites!'
-                          : 'Meal removed from favorites!',
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Toggle favorite status
+              final isExisting = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleMealFavoriteStatus(meal);
+
+              // Show a snackbar based on the result
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    isExisting
+                        ? 'Meal added to favorites!'
+                        : 'Meal removed from favorites!',
                   ),
-                );
-              },
-              icon: isFavorite
-                  ? const Icon(Icons.star)
-                  : const Icon(Icons.star_border))
-        ]),
-        body: SingleChildScrollView(
-            padding: const EdgeInsets.all(25),
-            child: Column(children: [
-              Image.network(
-                meal.imageUrl,
-                height: 300,
-                width: double.infinity,
-                fit: BoxFit.cover,
+                ),
+              );
+            },
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) => RotationTransition(
+                turns: Tween(begin: 0.8, end: 1.0).animate(animation),
+                child: child,
               ),
-              const SizedBox(height: 14),
+              child: isFavorite
+                  ? const Icon(
+                      Icons.star,
+                      key: ValueKey('full_star'),
+                    )
+                  : const Icon(
+                      Icons.star_border,
+                      key: ValueKey('empty_star'),
+                    ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(0),
+        child: Column(
+          children: [
+            Hero(
+              tag: meal.id,
+              child: Material(
+                color: Colors.transparent,
+                child: TweenAnimationBuilder<double>(
+                  duration:
+                      const Duration(seconds: 1), // Extend animation duration
+                  tween: Tween<double>(begin: 0.7, end: 1.0),
+                  builder: (context, scale, child) => Transform.scale(
+                    scale: scale,
+                    child: child,
+                  ),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 300,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 14),
+            Text(
+              'Ingredients:',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 20),
+            for (final ingredient in meal.ingredients)
               Text(
-                'Ingredients:',
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontWeight: FontWeight.bold,
+                ingredient,
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
                     ),
               ),
-              const SizedBox(height: 20),
-              for (final ingredient in meal.ingredients)
-                Text(
-                  ingredient,
+            const SizedBox(height: 20),
+            Text(
+              'Steps:',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            const SizedBox(height: 20),
+            for (final step in meal.steps)
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8),
+                child: Text(
+                  step,
                   style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                         color: Theme.of(context).colorScheme.onBackground,
                       ),
+                  textAlign: TextAlign.center,
                 ),
-              const SizedBox(height: 20),
-              Text(
-                'Steps:',
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontWeight: FontWeight.bold,
-                    ),
               ),
-              const SizedBox(height: 20),
-              for (final step in meal.steps)
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8),
-                  child: Text(
-                    step,
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                          color: Theme.of(context).colorScheme.onBackground,
-                        ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              const SizedBox(height: 100),
-            ])));
+            const SizedBox(height: 100),
+          ],
+        ),
+      ),
+    );
   }
 }
